cb976310da6e025cb37d379436982de2b57ac8d8
andrew34765 <54000146+andrew34765@users.noreply.github.com>
2020-05-09 12:39:46 -0500

    Update equipment.js

--- modules/equipment.js
+++ modules/equipment.js
@@ -4,7 +4,7 @@ MODULES["equipment"].numHitsSurvived = 60;   //survive X hits in D stance or not
 MODULES["equipment"].numHitsSurvivedScry = 80;
 MODULES["equipment"].enoughDamageCutoff = 4; //above this the game will buy attack equipment
 MODULES["equipment"].capDivisor = 10; //Your Equipment cap divided by this will give you the lower cap for liquified and overkilled zones
-MODULES["equipment"].alwaysLvl2 = true; //Always buys the 2nd level of equipment. Its the most effective.
+MODULES["equipment"].alwaysLvl2 = false; //Always buys the 2nd level of equipment. Its the most effective.
 MODULES["equipment"].waitTill60 = true; // 'Skip Gear Level 58&59', 'Dont Buy Gear during level 58 and 59, wait till level 60, when cost drops down to 10%
 MODULES["equipment"].equipHealthDebugMessage = false;    //this repeats a message when you don't have enough health. set to false to stop the spam.
 
